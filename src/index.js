0dd2d587c617ff4213f451801fa5655c1f4de98d
Mariano <marianonunezma@gmail.com>
2022-06-27 19:41:12 -0300

    Design Ready

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './scss/style.scss';
 import 'bootstrap';
+
+const data = async () => {
+    try {
+        const raesolve = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=79a239672389bc1c02633593b74da9e2');
+        const result = await raesolve.json();
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+console.log(data());
+
